df8c4d7efe972d56dfe6c3dff569d0da83146b4c
Rolandas <kazrolandas@gmail.com>
2018-04-23 20:00:27 +0300

    core app page navigation through tab name

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Header from "./components/Header";
+import Orders from "./components/Orders";
+// import Settings from "./components/Settings";
+import Stats from "./components/Stats";
+import Settings from "./components/Settings";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        body
-      </div>
-    );
-  }
+    state = {
+        tabs: ['Orders', 'Statistics', 'Settings'],
+        activeTab: 'Orders',
+    };
+
+    switchTab = (activeTab) => this.setState({activeTab});
+
+    renderContent = () => {
+        switch (this.state.activeTab) {
+            case 'Orders' :
+                return <Orders/>;
+            case 'Statistics' :
+                return <Stats/>;
+            case 'Settings' :
+                return <Settings/>;
+            default : return null
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <Header switchTab={this.switchTab} tabs={this.state.tabs}/>
+                {this.renderContent()}
+
+            </div>
+        );
+    }
 }
 
 export default App;
